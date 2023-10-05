bebd327b8759c241b618d790a74ce9820f91db67
tarek.habash <tarek.habash@zooom.com>
2023-10-05 09:26:41 +0200

    fetch cabin data

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -2,7 +2,7 @@ import supabase from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
-  
+
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be loaded");
