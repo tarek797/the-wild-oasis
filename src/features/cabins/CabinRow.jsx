1f0936e7905cf23f088c34aac6841435b3b7e5c3
tarek.habash <tarek.habash@zooom.com>
2023-10-26 16:43:27 +0200

    build a reusable table

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -6,23 +6,13 @@ import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+import Table from "../../ui/Table";
 
 const Img = styled.img`
   display: block;
   width: 6.4rem;
   aspect-ratio: 3 / 2;
-  object-fit: cover;
+  object-fit: cover; 
   object-position: center;
   transform: scale(1.5) translateX(-7px);
 `;
@@ -71,7 +61,7 @@ function CabinRow({ cabin }) {
 
   return (
     <>
-      <TableRow role="row">
+      <Table.Row role="row">
         <Img src={image} />
         <Cabin>{name}</Cabin>
         <div>Fits up tp {maxCapacity} guests</div>
@@ -94,22 +84,21 @@ function CabinRow({ cabin }) {
             <Modal.Window name="edit">
               <CreateCabinForm cabinToEdit={cabin} />
             </Modal.Window>
-            <Modal.Open>
-            <button >
-              <HiTrash />
-            </button>
+            <Modal.Open opens="delete">
+              <button>
+                <HiTrash />
+              </button>
             </Modal.Open>
-            <Modal.Window>
+            <Modal.Window name="delete">
               <ConfirmDelete
-              resourceName="cabins"
-              disabled={isDeleting}
-              onConfirm={() => deleteCabin(cabinId)}
+                resourceName="cabins"
+                disabled={isDeleting}
+                onConfirm={() => deleteCabin(cabinId)}
               />
             </Modal.Window>
-            
           </Modal>
         </div>
-      </TableRow>
+      </Table.Row>
     </>
   );
 }
