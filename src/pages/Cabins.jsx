ca935af476f07992a166b047efa55d36b15c17d2
tarek.habash <tarek.habash@zooom.com>
2023-11-02 15:46:51 +0100

    add filter cabin feature

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,14 +2,14 @@ import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinsTableOperations from "../features/cabins/CabinsTableOperations";
 
 function Cabins() {
-
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinsTableOperations />
       </Row>
       <Row>
         <CabinTable />
