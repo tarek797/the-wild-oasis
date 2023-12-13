43c988b4ab27ea1a19080f6bb4fbd0f3486d728c
tarek.habash <tarek.habash@zooom.com>
2023-12-13 18:06:52 +0100

    compute recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBooking";
 
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -7,6 +10,11 @@ const StyledDashboardLayout = styled.div`
   gap: 2.4rem;
 `;
 function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+  console.log(bookings);
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
