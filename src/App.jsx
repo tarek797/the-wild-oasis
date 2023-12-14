a460e9575f89d425ff2874167d2b73030406e16d
tarek.habash <tarek.habash@zooom.com>
2023-12-14 15:32:44 +0100

    handle Error Boundaries

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route
+          <Route 
             element={
               <ProtectedRoute>
                 <AppLayout />
